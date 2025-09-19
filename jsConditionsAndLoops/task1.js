025ad1ec43ec84842570b719f96427a8750ddc79
Helga <olga.korotash@gmail.com>
2025-09-19 20:21:21 +0300

    fix: simplified the conditions in task1, task2

--- jsConditionsAndLoops/task1.js
+++ jsConditionsAndLoops/task1.js
@@ -1,13 +1,13 @@
 const averageGrade = 33; // from 0 to 100 only
 
-if (averageGrade>=60 && averageGrade<=70){
-    console.log("Задовільно");
-} else if (averageGrade>70 && averageGrade<=80){
-    console.log("Добре");
-} else if (averageGrade>80 && averageGrade<=90){
-    console.log("Дуже добре");
-} else if (averageGrade>90 && averageGrade<=100){
+if (averageGrade>90){
     console.log("Відмінно");
+} else if (averageGrade>80){
+    console.log("Дуже добре");
+} else if (averageGrade>70){
+    console.log("Добре");
+} else if (averageGrade>=60){
+    console.log("Задовільно");
 } else {
     console.log("Незадовільно");
 }
